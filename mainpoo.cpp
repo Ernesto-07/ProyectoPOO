6d331981ff4df4fa451bb50972b05466685fa84f
Ernesto-07 <a01710199@tec.mx>
2025-11-28 10:25:28 -0600

    Update mainpoo.cpp

--- mainpoo.cpp
+++ mainpoo.cpp
@@ -5,15 +5,23 @@ using namespace std;
 #include "Taller.h"
 
 int main() {
+
+    Taller t1("Taller Don Paco");
+
     Vehiculo v1("Nissan", "Versa");
-    Mecanico m1("Carlos");
+    Cliente c1("Luis", "Pérez", 30, 5);
+    Mecanico m1("Carlos", "Hernandez", 40, 10);
     Servicio s1("Cambio de aceite", 500);
 
-    cout << "Vehiculo: " << v1.get_marca() << endl;
-    cout << "Mecanico: " << m1.get_nombre() << endl;
-    cout << "Servicio: " << s1.get_tipo() << endl;
+    t1.agregarCliente(c1);
+    t1.agregarServicio(s1);
+    t1.agregarVehiculo("Nissan", "Versa");
 
     v1.describir();
     m1.bienvenida();
     s1.describir();
+
+    t1.describir();
+
+    return 0;
 }
