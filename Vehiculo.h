33ee618d72c13da10b83ef0580d74499e022722e
Ernesto-07 <a01710199@tec.mx>
2025-12-03 08:42:20 -0600

    Enhance Vehiculo class with documentation comments
    Added documentation comments to the Vehiculo class and its methods.

--- Vehiculo.h
+++ Vehiculo.h
@@ -1,3 +1,9 @@
+/*
+ * Clase Vehiculo
+ * Representa un vehículo dentro del taller.
+ * Contiene marca y modelo.
+ */
+
 #ifndef VEHICULO_H
 #define VEHICULO_H
 
@@ -7,23 +13,27 @@ using namespace std;
 
 class Vehiculo {
 private:
-    string marca;
-    string modelo;
+    string marca;  // marca del vehiculo
+    string modelo; // modelo del vehiculo
 
 public:
+    // Constructor por defecto: inicializa con valores vacios
     Vehiculo() {
         marca = "";
         modelo = "";
     }
 
+    // Constructor con paametros: inicializa con valores dados
     Vehiculo(string m, string mod) {
         marca = m;
         modelo = mod;
     }
 
+    // Getters para acceder a los atributos
     string get_marca() { return marca; }
     string get_modelo() { return modelo; }
 
+    // Metodo para mostrar informacion del vehiculo
     void describir() {
         cout << "Vehiculo: " << marca << " " << modelo << endl;
     }
