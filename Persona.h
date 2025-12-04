03212212ae4d7a4ec875db96319a5cc725d0a278
Ernesto-07 <a01710199@tec.mx>
2025-12-03 20:14:41 -0600

    Update Persona.h

--- Persona.h
+++ Persona.h
@@ -19,22 +19,75 @@ protected: // protegido para que las subclases puedan acceder
 
 public:
     // Constructor con parametros: inicializa con valores dados
-    Persona(string n, string a, int e) {
+    Persona(string n, string a, int e);
+
+    // Getters para acceder a los atributos
+    string getNombre();
+
+    string getApellido();
+
+    int getEdad();
+
+    // Método común para mostrar información de la persona
+    void describir();
+};
+
+
+/**
+ * Constructor donde recibe valores para llenar las variables 
+ *
+ * @param string n: nombre, string a: apellido, int e: edad,
+ * @return
+*/
+Persona :: Persona(string n, string a, int e){
         nombre = n;
         apellido = a;
         edad = e;
     }
 
-    // Getters para acceder a los atributos
-    string get_nombre() { return nombre; }
-    string get_apellido() { return apellido; }
-    int get_edad() { return edad; }
 
-    // Método común para mostrar información de la persona
-    void describir() {
+/**
+ * getter de nombre
+ *
+ * @param
+ * @return string: nombre
+*/      
+string Persona :: getNombre(){ 
+        return nombre;
+    }
+
+
+/**
+ * getter de apellido
+ *
+ * @param
+ * @return string: apellido
+*/      
+string Persona :: getApellido(){ 
+        return apellido;
+    }
+
+
+/**
+ * getter de edad
+ *
+ * @param
+ * @return int: edad
+*/      
+int Persona :: getEdad(){ 
+        return edad;
+    }
+
+    
+/**
+ * void de describir
+ *
+ * @param
+ * @return 
+*/      
+void Persona :: describir(){
         cout << "Persona: " << nombre << " " << apellido
              << " | Edad: " << edad << endl;
     }
-};
 
 #endif
