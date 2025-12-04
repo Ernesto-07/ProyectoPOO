3ecec6d194139d1a3fceef9c5683113cc79bfac3
Ernesto-07 <a01710199@tec.mx>
2025-12-03 20:13:51 -0600

    Update Cliente.h
    Updated class Cliente to improve variable naming and added method definitions.

--- Cliente.h
+++ Cliente.h
@@ -11,30 +11,84 @@
 #include <iostream>
 using namespace std;
 
+//Declaracion de clase cliente que hereda de persona
 class Cliente : public Persona {
 private:
-    int anos_uso;  // años de uso del taller
+    //DECLARACION DE VARIABLES
+    int anosUso;  // años de uso del coche
     int cantidad;  // cantidad de servicios realizados
 
 public:
+    //DECLARACION DE LOS METODOS QUE TENDRA EL OBJETO
+
     // Constructor con parametros: inicializa atributos de Persona y Cliente
-    Cliente(string n, string a, int e, int au, int c)
-        : Persona(n, a, e) { // llamada al constructor de Persona
-        anos_uso = au;
+    Cliente(string n, string a, int e, int au, int c);
+
+    // Getters para acceder a los atributos
+    int getAnosUso(); 
+
+    int getCantidad();
+
+    // Método para mostrar información del cliente
+    void describir();
+
+    
+};
+
+// DEFINICIONES DE CADA METODO
+
+
+/**
+ * Constructor donde recibe valores para llenar las variables
+ *
+ * @param string n: el nombre del cliente, string a: el apellido del cliente,
+ *        int e: la edad, int au: anos de uso coche, int c: cantidad de servicios
+ * @return
+*/
+Cliente :: Cliente (string n, string a, int e, int au, int c) 
+    : Persona(n, a, e){ // llamada al constructor de Persona
+        anosUso = au;
         cantidad = c;
     }
 
-    // Getters para acceder a los atributos
-    int get_anos_uso() { return anos_uso; }
-    int get_cantidad() { return cantidad; }
 
-    // Método para mostrar información del cliente
-    void describir() {
+
+//Getter de anos uso y cantidad de servicios
+
+/**
+ * getter anos uso
+ *
+ * @param
+ * @return int: anos de uso
+*/
+int Cliente :: getAnosUso() { 
+        return anosUso; 
+    }
+
+
+/**
+ * getter cantidad servicios
+ *
+ * @param
+ * @return int: cantidad de servicios
+*/    
+int Cliente :: getCantidad(){ 
+        return cantidad;
+    }
+
+
+
+/**
+ * void describir
+ * no regresa nada 
+ * @param
+ * @return 
+*/      
+void Cliente :: describir(){
         cout << "Cliente: " << nombre << " " << apellido
              << " | Edad: " << edad
-             << " | Anos de uso: " << anos_uso //años(se puso anos por impresion)
+             << " | Anos de uso: " << anosUso //años(se puso anos por impresion)
              << " | Servicios realizados: " << cantidad << endl;
     }
-};
 
 #endif
